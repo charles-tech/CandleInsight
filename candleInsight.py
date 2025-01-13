40a9d63ba471ac04c2fb139632d7a2eff5d2b14f
Charles Miranda <54559060+charles-tech@users.noreply.github.com>
2025-01-13 11:16:31 -0300

    Update candleInsight.py

--- candleInsight.py
+++ candleInsight.py
@@ -4,19 +4,35 @@ import streamlit as st
 import plotly.graph_objects as go
 
 def download_data(ticker, start_date, end_date):
-    return yf.download(ticker, start=start_date, end=end_date)
+    try:
+        data = yf.download(ticker, start=start_date, end=end_date)
+        if data.empty:
+            st.warning(f"Nenhum dado foi retornado para o ticker {ticker}.")
+        return data
+    except Exception as e:
+        st.error(f"Erro ao baixar dados: {e}")
+        return pd.DataFrame()
+
+def check_data_columns(data):
+    st.write(f"Colunas disponíveis: {', '.join(data.columns)}")
 
 def transform_data(data):
-    data['Month-Year'] = data.index.to_period('M')
-    monthly_data = data.resample('M').agg({'Open': 'first', 'High': 'max', 'Low': 'min', 'Close': 'last'})
-    return monthly_data
+    try:
+        monthly_data = data.resample('M').agg({'Open': 'first', 
+                                               'High': 'max', 
+                                               'Low': 'min', 
+                                               'Close': 'last'})
+        return monthly_data
+    except KeyError as ke:
+        st.error(f"Erro ao realizar agregação dos dados. Erro: {ke}")
+        return None
 
 def display_data_in_streamlit(ticker, start_date, end_date, monthly_data):
     overall_min = monthly_data['Low'].min()
     overall_max = monthly_data['High'].max()
 
     st.write(f"Valores mensais para {ticker.upper()} entre {start_date} e {end_date}")
-    monthly_summary = monthly_data[['Low', 'High']].rename(columns={'Low': 'Min_Close', 'High': 'Max_Close'})
+    monthly_summary = monthly_data[['Open', 'Low', 'High', 'Close']]
     monthly_summary.index = monthly_summary.index.strftime('%d/%m/%Y')
     st.table(monthly_summary)
 
@@ -45,17 +61,24 @@ def display_data_in_streamlit(ticker, start_date, end_date, monthly_data):
 
 def main():
     st.title("Yahoo Finance Data Viewer")
-
-    # Inputs do usuário
+    
     ticker = st.text_input("Digite o ticker do ativo:")
     start_date = st.date_input("Digite a data inicial:")
     end_date = st.date_input("Digite a data final:")
 
     if ticker and start_date and end_date:
         data = download_data(ticker, start_date, end_date)
+        
         if not data.empty:
-            monthly_data = transform_data(data)
-            display_data_in_streamlit(ticker, start_date, end_date, monthly_data)
+            if {'Open', 'High', 'Low', 'Close'}.issubset(data.columns):
+                monthly_data = transform_data(data)
+                if monthly_data is not None:
+                    display_data_in_streamlit(ticker, start_date, end_date, monthly_data)
+                else:
+                    st.write("Erro na transformação dos dados.")
+            else:
+                st.write("Um ou mais campos de dados estão faltando. Confira a integridade dos dados.")
+                check_data_columns(data)
         else:
             st.write("Não foi possível recuperar dados para o ticker especificado.")
 
